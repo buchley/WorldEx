7fd942babab8922ae89a30ccb12909c6beca46cf
GB <ultraturbomotive@gmail.com>
2018-02-20 17:13:59 +0900

    commit

--- HelloGitWorld.java
+++ HelloGitWorld.java
@@ -1,5 +1,7 @@
 public class HelloGitWorld{
 	public static void main(String[] args){
 		System.out.println("Git Test");
+		System.out.println("Git Remote Test");
+		System.out.println("Git Local Modifying");
 	}	
 }
